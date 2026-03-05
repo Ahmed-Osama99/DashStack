906b5fd6e881a9d178ad02eeca6a9f5ac2178d46
Ahmed Osama <ahmed.osama.web@outlook.com>
2026-03-05 16:34:47 +0200

    Change base path from '/Dashboard/' to '/DashStack/'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Dashboard/",
+  base: "/DashStack/",
   plugins: [react(), tailwindcss()],
 });
